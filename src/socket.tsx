c8556c4c2e851208ac4ff407420e4509cc93d6bd
Anime1026 <sasuke031026@gmail.com>
2023-05-23 15:22:58 +0300

    change url

--- src/socket.tsx
+++ src/socket.tsx
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'http://192.168.115.166:5000/plinko';
+const URL = 'http://54.226.42.122';
 
 export const socket = io(URL);
