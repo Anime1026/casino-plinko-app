73739543d9368d0b4892217a9e813b57f1c7bdc1
Anime1026 <sasuke031026@gmail.com>
2023-07-19 22:02:47 +0300

    change font size

--- src/socket.tsx
+++ src/socket.tsx
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'http://54.226.42.122';
-// const URL = 'http://192.168.115.166:5000';
+// const URL = 'http://54.226.42.122';
+const URL = 'http://192.168.115.178:5007';
 
 export const socket = io(URL);
